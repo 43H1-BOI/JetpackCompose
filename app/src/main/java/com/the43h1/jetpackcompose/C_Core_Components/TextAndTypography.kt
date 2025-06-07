b0773564fdd98368c8aece151bae78f232d2dec7
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-07 14:38:11 +0530

    Added Some Files

--- app/src/main/java/com/the43h1/jetpackcompose/C_Core_Components/TextAndTypography.kt
+++ app/src/main/java/com/the43h1/jetpackcompose/C_Core_Components/TextAndTypography.kt
@@ -1,14 +1,35 @@
 package com.the43h1.jetpackcompose.C_Core_Components
 
+import android.graphics.BlurMaskFilter.Blur
+import android.util.Size
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.painter.BrushPainter
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
+import com.the43h1.jetpackcompose.R
 
 @Composable
 fun SimpleText() {
@@ -19,14 +40,23 @@ fun SimpleText() {
     {
         Text(
             text = "Hello Jetpack Compose",
-            textAlign = TextAlign.Center ,
-            color = colorResource(R.color.mera_color)
+            textAlign = TextAlign.Center,
+            color = colorResource(R.color.black),
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = colorResource(R.color.mera_color),
+                    blurRadius = 6f
+                )
+            )
         )
     }
 }
 
 @Composable
-fun RainbowText(){
+fun RainbowText() {
     val rainbow = listOf(
         colorResource(R.color.violet),
         colorResource(R.color.indigo),
@@ -50,14 +80,15 @@ fun RainbowText(){
                 withStyle(
                     SpanStyle(
                         brush = Brush.verticalGradient(
-                            colors = rainbow ,
+                            colors = rainbow,
                             endY = 135f
                         )
                     )
                 ) {
                     append("Interfere in Anyone's Matter\n")
+                    append("I am here to build my own Empire\n")
                 }
-                append("I am here to build my own Empire")
+                append("Samjhe ?!")
             }
         )
     }
@@ -82,5 +113,8 @@ fun ScrollableText() {
 @Preview(showSystemUi = true)
 @Composable
 fun TextPreview() {
-    SimpleText()
+    // Uncomment Below Lines to Check Their Previews
+//    SimpleText()
+//    RainbowText()
+//    ScrollableText()
 }
