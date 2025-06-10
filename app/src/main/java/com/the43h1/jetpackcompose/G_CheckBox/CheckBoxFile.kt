2af9b6597fb99cb875ec3c716b7e5763b92e76dd
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-10 11:02:22 +0530

    Added CheckBox

--- app/src/main/java/com/the43h1/jetpackcompose/G_CheckBox/CheckBoxFile.kt
+++ app/src/main/java/com/the43h1/jetpackcompose/G_CheckBox/CheckBoxFile.kt
@@ -5,16 +5,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
-import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,26 +27,7 @@ import androidx.compose.ui.unit.sp
 fun CheckBoxEx() {
     val context = LocalContext.current
 
-    // Having multiple checkbox default values
-    val childCheckList = remember {
-        mutableListOf(false, false, false)
-    }
-
-    // A Parent which get selected when all are selected,
-    // unselected when none are selected And
-    // shows partial selection when some of members are selected
-
-    var parentCheckList = {
-        when {
-            childCheckList.all { it } -> ToggleableState.On
-            childCheckList.none() -> ToggleableState.Off
-            childCheckList.any() -> ToggleableState.Indeterminate
-            //          OR
-            // else -> ToggleableState.Indeterminate
-        }
-    }
-
-
+    var listItems: List<String> = listOf("Option 1", "Option 2", "Option 3")
 
 
     Column(
@@ -55,108 +38,39 @@ fun CheckBoxEx() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Row(
-            modifier = Modifier
-                .padding(top = 5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-//            Checkbox(
-//                checked = false,
-//                modifier = Modifier.background(Color.Cyan),
-//                enabled = false,
-//                colors = CheckboxDefaults.colors(Color.Black),
-//                onCheckedChange = TODO(),
-//                interactionSource = TODO()
-//            )
-            TriStateCheckbox(
-                state = ToggleableState.Off,
-                onClick = {
-                    val newState = parentCheckList != ToggleableState.On
 
-                    childCheckList.forEachIndexed { index, _ ->
-                        childCheckList[index] = newState
-                    }
-//                    Toast.makeText(context, "All Selected", Toast.LENGTH_SHORT).show()
-                },
-                modifier = TODO(),
-                enabled = TODO(),
-                colors = TODO(),
-                interactionSource = TODO(),
-            )
+        Text(
+            text = "Select Items",
+            textAlign = TextAlign.Left,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
 
-            Text(
-                "Select All",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Left
-            )
-        }
-        Row(
-            modifier = Modifier
-                .padding(top = 5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-//            Checkbox(
-//                checked = false,
-//                modifier = Modifier.background(Color.Cyan),
-//                enabled = false,
-//                colors = CheckboxDefaults.colors(Color.Black),
-//                onCheckedChange = TODO(),
-//                interactionSource = TODO()
-//            )
+        listItems.forEach { eachItems ->
+            var checkedItem by remember {
+                mutableStateOf(false)
+            }
 
-            Text(
-                "Option 1",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Left
-            )
-        }
-        Row(
-            modifier = Modifier
-                .padding(top = 5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-//            Checkbox(
-//                checked = false,
-//                modifier = Modifier.background(Color.Cyan),
-//                enabled = false,
-//                colors = CheckboxDefaults.colors(Color.Black),
-//                onCheckedChange = TODO(),
-//                interactionSource = TODO()
-//            )
+            Row {
+                Checkbox(
+                    checked = checkedItem,
+                    onCheckedChange =
+                        { checkedStatus ->
+                            checkedItem = checkedStatus
+                        },
+                    modifier = Modifier
+                        .size(27.dp)
+                )
 
-            Text(
-                "Option 2",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Left
-            )
-        }
-        Row(
-            modifier = Modifier
-                .padding(top = 5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-//            Checkbox(
-//                checked = false,
-//                modifier = Modifier.background(Color.Cyan),
-//                enabled = false,
-//                colors = CheckboxDefaults.colors(Color.Black),
-//                onCheckedChange = TODO(),
-//                interactionSource = TODO()
-//            )
+                Text(
+                    text = eachItems,
+                    textAlign = TextAlign.Left,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
 
-            Text(
-                "Option 3",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Left
-            )
+
         }
     }
 }
