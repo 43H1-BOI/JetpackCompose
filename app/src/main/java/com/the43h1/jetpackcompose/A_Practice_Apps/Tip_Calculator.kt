5da6c0c689ec1915b799e77f4be89849268e2dd6
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-11 11:00:34 +0530

    Added Tip Calculator

---
+++ app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Tip_Calculator.kt
@@ -0,0 +1,240 @@
+package com.the43h1.jetpackcompose.A_Practice_Apps
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.the43h1.jetpackcompose.R
+import kotlin.math.roundToInt
+
+// To Calculate Tip Logic for Tip Layout 2
+fun calcTipLogic(costOfService: Int, tipPercent: Int) =
+    (costOfService * (tipPercent.toFloat() / 100))
+
+
+@Composable
+fun LayoutTip1() {
+
+    var tip by remember {
+        mutableFloatStateOf(0.0f)
+    }
+
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxWidth()
+            .padding(10.dp),
+//            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.tipCalc),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+//                .align(Alignment.Start)
+        )
+
+        TextField(
+            value = tip.toString(),
+            onValueChange = {
+                tip = it.toFloat()
+            },
+            label = {
+                Text("Enter Your Tip")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal
+            ),
+//            colors = TextFieldDefaults.colors(Color.Red,Color.Blue),
+            modifier = Modifier
+                .padding(top = 30.dp)
+//                .border(
+//                    width = 2.dp,
+//                    brush = Brush.linearGradient(
+//                        colors = listOf(
+//                            Color.Red,
+//                            Color.Green,
+//                            Color.Blue
+//                        )
+//                    ), shape = RectangleShape
+//                )
+        )
+
+        Text(
+            text =
+                if (tip != 0.0f) {
+                    "Thanks for \u20B9${tip.toString()} Tip \nMay You Got All Happiness in Your Life"
+                } else "Please Enter a Tip Amount :)",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 20.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LayoutTip2() {
+
+    var costOfService: Int by remember {
+        mutableIntStateOf(0)
+    }
+
+    var tipPercent: Int by remember {
+        mutableIntStateOf(0)
+    }
+
+    var toogleSwitch: Boolean by remember {
+        mutableStateOf(false)
+    }
+
+    var tip: Float by remember {
+        mutableFloatStateOf(0.0f)
+    }
+
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxWidth()
+//            .background(Color.LightGray)
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.tipCalc),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+        )
+
+        TextField(
+            value = costOfService.toString(),
+            onValueChange = {
+                costOfService = it.toInt()
+            },
+            label = {
+                Text("Cost of Service")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal
+            ),
+            modifier = Modifier
+                .padding(top = 30.dp)
+        )
+
+        TextField(
+            value = tipPercent.toString(),
+            onValueChange = {
+                tipPercent = it.toInt()
+            },
+            label = {
+                Text("Tip(%)")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal
+            ),
+            modifier = Modifier
+                .padding(top = 15.dp)
+        )
+        Row(
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                "Round Off Tip?",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+//                    .align(Alignment.CenterVertically)
+//                    .padding(start = 10.dp)
+            )
+            Switch(
+                checked = toogleSwitch,
+                onCheckedChange = {
+                    toogleSwitch = !toogleSwitch
+                    tip.roundToInt()
+                }, thumbContent = {
+                    AnimatedVisibility(toogleSwitch) {
+                        Icon(Icons.Default.Done, null)
+                    }
+
+//                    ?else
+//                    Icon(Icons.Default.Close, null)
+                }, modifier = Modifier
+//                    .height(10.dp)
+//                    .size(200.dp)
+            )
+        }
+
+        Text(
+            text =
+                if (costOfService == 0) "Enter Cost of Service"
+                else if (tipPercent == 0) "Enter Tip Percentage"
+                else {
+                    tip = calcTipLogic(
+                        costOfService,
+                        tipPercent
+                    )
+                    "Thanks for \u20B9${
+                        if (!toogleSwitch) tip
+                        else tip.roundToInt()
+                    } Tip \nMay You Got All Happiness in Your Life"
+                },
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 20.dp)
+        )
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewFun() {
+    LayoutTip2()
+}
