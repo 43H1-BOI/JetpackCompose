eb3bb4d3c96cdf96e7457de79c61620eebfd66ee
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-21 16:59:23 +0530

    Unit Converter App : Added Enum Class

--- app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Unit_Converter_7/UnitConverterApp.kt
+++ app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Unit_Converter_7/UnitConverterApp.kt
@@ -33,16 +33,24 @@ import androidx.compose.ui.unit.sp
 @Preview(showSystemUi = true)
 @Composable
 fun UnitConv() {
-    var userInput by remember {
-        mutableStateOf("0".toIntOrNull())
-    }
+    var userInput by remember { mutableStateOf("0".toInt()) }
+
+    var drawer1 by remember { mutableStateOf(false) }
+    var drawer2 by remember { mutableStateOf(false) }
+
+    var inputUnit by remember { mutableStateOf(Units.Centimeter) }
+    var outputUnit by remember { mutableStateOf(Units.Meter) }
+
+    var result = 0.0
+
+
 
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 20.dp, end = 20.dp)
+            .padding(start = 20.dp, end = 20.dp, top = 50.dp)
     ) {
         Text(
             text = "Unit Converter App",
@@ -53,24 +61,40 @@ fun UnitConv() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = try {
+                    userInput.toString()
+                } catch (_: NumberFormatException) {
+                    "0"
+                } catch (e: Exception) {
+                    "null + ${e.message}"
+                },
+                onValueChange = {
+                    userInput = try {
+                        it.toInt()
+                    } catch (e: NumberFormatException) {
+                        0
+                    }
+                }, modifier = Modifier
+                    .fillMaxWidth(0.7f)
+            )
 
-        OutlinedTextField(
-            value = try {
-                userInput.toString()
-            } catch (e: NumberFormatException) {
-                "0"
-            } catch (e: Exception) {
-                "null + ${e.message}"
-            },
-            onValueChange = {
-                userInput = try {
-                    it.toInt()
-                } catch (e: NumberFormatException) {
-                    0
-                }
-            }, modifier = Modifier.fillMaxWidth()
-        )
+            Spacer(modifier = Modifier.width(8.dp))
 
+            Button(
+                onClick = {
+                    result = convert(
+                        inputUnit = inputUnit,
+                        outputUnit = outputUnit,
+                        inputValue = userInput
+                    )
+                },
+            ) { Text("Convert") }
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -82,23 +106,49 @@ fun UnitConv() {
 
             Box(modifier = Modifier.weight(1f)) {
                 Button(
-                    onClick = { }, modifier = Modifier
+                    onClick = { drawer1 = true }, modifier = Modifier
                         .align(Alignment.Center)
                         .fillMaxWidth(0.8f)
                 ) {
-                    Text("Unit")
+                    Text(inputUnit.toString(), fontSize = 14.sp)
                     Icon(Icons.Default.ArrowDropDown, "Drop Down")
                 }
 
                 DropdownMenu(
-                    expanded = false, onDismissRequest = { }, offset = DpOffset(23.dp, 0.dp)
+                    expanded = drawer1,
+                    onDismissRequest = {
+                        drawer1 = false
+                    },
+                    offset = DpOffset(23.dp, 0.dp)
                 ) {
-                    DropdownMenuItem(text = { Text("Millimeter") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Centimeter") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Meter") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Kilometer") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Foot") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Inch") }, onClick = {})
+                    DropdownMenuItem(
+                        text = { Text(Units.Millimeter.toString()) },
+                        onClick = {
+                            drawer1 = false
+                            outputUnit = Units.Millimeter
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(Units.Centimeter.toString()) },
+                        onClick = {
+                            drawer1 = false
+                            outputUnit = Units.Centimeter
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(Units.Meter.toString()) },
+                        onClick = {
+                            drawer1 = false
+                            outputUnit = Units.Meter
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(Units.Kilometer.toString()) },
+                        onClick = {
+                            drawer1 = false
+                            outputUnit = Units.Kilometer
+                        }
+                    )
                 }
             }
 
@@ -106,25 +156,94 @@ fun UnitConv() {
 
             Box(modifier = Modifier.weight(1f)) {
                 Button(
-                    onClick = { }, modifier = Modifier
+                    onClick = { drawer2 = true }, modifier = Modifier
                         .align(Alignment.Center)
                         .fillMaxWidth(0.8f)
                 ) {
-                    Text("Unit")
+                    Text(outputUnit, fontSize = 14.sp)
                     Icon(Icons.Default.ArrowDropDown, "Drop Down")
                 }
 
                 DropdownMenu(
-                    expanded = false, onDismissRequest = { }, offset = DpOffset(23.dp, 0.dp)
+                    expanded = drawer2,
+                    onDismissRequest = {
+                        drawer2 = false
+                    },
+                    offset = DpOffset(23.dp, 0.dp)
                 ) {
-                    DropdownMenuItem(text = { Text("Millimeter") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Centimeter") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Meter") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Kilometer") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Foot") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Inch") }, onClick = {})
+
+                    DropdownMenuItem(
+                        text = { Text("Millimeter") },
+                        onClick = {
+                            drawer2 = false
+                            inputUnit = Units.Millimeter
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Centimeter") },
+                        onClick = {
+                            drawer2 = false
+                            inputUnit = "Centimeter"
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meter") },
+                        onClick = {
+                            drawer2 = false
+                            inputUnit = "Meter"
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Kilometer") },
+                        onClick = {
+                            drawer2 = false
+                            inputUnit = "Kilometer"
+                        }
+                    )
                 }
             }
         }
+
+        if (result != 0.0) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                "Result : $result",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp
+            )
+        }
     }
+}
+
+
+fun convert(inputUnit: Units, outputUnit: Units, inputValue: Int): Double {
+    return when (inputUnit) {
+        Units.Millimeter ->
+            when (outputUnit) {
+                Units.Centimeter -> (inputValue / 10).toDouble()
+                Units.Meter -> (inputValue / 100).toDouble()
+                Units.Kilometer -> (inputValue / 1000).toDouble()
+                Units.Millimeter -> inputValue.toDouble()
+            }
+
+        Units.Centimeter ->
+            when (outputUnit) {
+                "Centimeter" -> (inputValue / 10).toDouble()
+                "Meter" -> (inputValue / 100).toDouble()
+                "Kilometer" -> (inputValue / 1000).toDouble()
+            }
+
+        else -> {
+            0.0
+        }
+    }
+}
+
+
+enum class Units() {
+    Millimeter,
+    Centimeter,
+    Meter,
+    Kilometer
 }
