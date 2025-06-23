e57f7c33e8d018239458bbedabe6714936052a89
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-23 10:20:33 +0530

    Added some importss

--- app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Movies_8/SideNavDrawer.kt
+++ app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Movies_8/SideNavDrawer.kt
@@ -1,11 +1,27 @@
 package com.the43h1.jetpackcompose.A_Practice_Apps.Movies_8
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +31,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun NavDrawerItem(
@@ -43,7 +62,6 @@ fun NavDrawerItem(
         )
     }
 }
-
 
 @Composable
 internal fun DrawerContent(
