0801fca269b1a23977458e0b2300498f8429e9b6
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-23 10:18:51 +0530

    Added some OptIn

--- app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Movies_8/MovieApp.kt
+++ app/src/main/java/com/the43h1/jetpackcompose/A_Practice_Apps/Movies_8/MovieApp.kt
@@ -1,38 +1,28 @@
 package com.the43h1.jetpackcompose.A_Practice_Apps.Movies_8
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -40,9 +30,11 @@ import androidx.navigation.compose.rememberNavController
 import com.the43h1.jetpackcompose.A_Practice_Apps.Movies_8.Screens.FavoriteScreen
 import com.the43h1.jetpackcompose.A_Practice_Apps.Movies_8.Screens.HomeScreen
 import com.the43h1.jetpackcompose.A_Practice_Apps.Movies_8.Screens.ProfileScreen
-import kotlinx.coroutines.launch
+
+const val appName = "Movilogy"
 
 @Preview(showSystemUi = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieApp() {
     /** Contains Nav Controller
