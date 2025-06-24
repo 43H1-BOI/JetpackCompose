aa708fba1eae24ba17684a1acaf2da88ad2de9ba
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-24 12:49:49 +0530

    Added ?

--- app/src/main/java/com/the43h1/jetpackcompose/C_Core_Components/JSuperScriptAndSubScript.kt
+++ app/src/main/java/com/the43h1/jetpackcompose/C_Core_Components/JSuperScriptAndSubScript.kt
@@ -1,31 +1,41 @@
 package com.the43h1.jetpackcompose.C_Core_Components
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
 fun SubScript(
     mainText: String = "A",
-    mainTextSize: TextUnit = 20.sp,
-    subScript: String = "x",
+    mainTextSize: TextUnit = MaterialTheme.typography.displayLarge.fontSize,
+    subScript: String = "X",
     subScriptSize: TextUnit = mainTextSize / 2
 ) {
-
     Text(
-        mainText,
-        fontSize = mainTextSize
-    )
-    Text(
-        subScript,
-        fontSize = subScriptSize,
-        textAlign = TextAlign.End, modifier = Modifier.padding(start = 10.dp)
+        buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontSize = mainTextSize
+                )
+            ) {
+                append(mainText)
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontSize = subScriptSize,
+                    baselineShift = BaselineShift.Subscript,
+                    fontWeight = FontWeight.Normal
+                )
+            )
+            {append(subScript) }
+        }
     )
 }
