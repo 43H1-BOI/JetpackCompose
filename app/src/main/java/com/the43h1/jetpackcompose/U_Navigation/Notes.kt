56a860958d64c52fe212dc45c207cd1bd92d09ad
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-06-25 09:54:03 +0530

    Added Navigation

---
+++ app/src/main/java/com/the43h1/jetpackcompose/U_Navigation/Notes.kt
@@ -0,0 +1,16 @@
+package com.the43h1.jetpackcompose.U_Navigation
+
+/** How To Navigate
+ *      navigation means to move from one screen to another
+ *      it can be done using
+ *          - navController -> used to give navigation functionality
+ *          - NavHost()     -> used to define path of screen where we will navigate to
+ *              - composable() fun is used to define keyword and route to screen
+ *              - composable(keyword){ screenFun() }
+ *          - NavGraph()
+ *
+ *
+ *
+ * Navigation Can be done using two ways
+ *
+ */
